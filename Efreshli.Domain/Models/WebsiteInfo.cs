73426e57144d94d005a55738a6fc3c09d7fdb6fc
Aya Khaled <aya.abdelkhalk30@gmail.com>
2025-08-03 17:19:09 +0300

    .

--- Efreshli.Domain/Models/WebsiteInfo.cs
+++ Efreshli.Domain/Models/WebsiteInfo.cs
@@ -10,11 +10,12 @@ namespace Efreshli.Domain.Models
         public string LogoUrl { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string Description { get; set; }
+        public string DescriptionAr { get; set; }
+        public string DescriptionEn { get; set; }
         public string Office { get; set; }
         public string Location { get; set; }
         public string FacebookLink { get; set; }
-        public string InstaLink { get; set; }
+        public string InstagramLink { get; set; }
         public string YoutubeLink { get; set; }
         public string LinkedinLink { get; set; }
     }
