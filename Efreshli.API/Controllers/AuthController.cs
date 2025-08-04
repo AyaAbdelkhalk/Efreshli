9e040225a4fd36c404a101584f6efcb02d891f14
Doha Ahmed Ezzat <132849296+DohaEzzat@users.noreply.github.com>
2025-08-04 15:26:58 +0300

    vv

---
+++ Efreshli.API/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Efreshli.Application.DTOs;
+using Efreshli.Application.DTOs.IdentityDTOs;
+using Efreshli.Application.Services.AuthServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Efreshli.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto dto)
+        {
+            var result = await _authService.RegisterAsync(dto);
+            return Ok(new { token = result });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+            return Ok(new { token = result });
+        }
+            
+    }
+}
