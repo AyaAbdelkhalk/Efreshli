55d8953815a25650120918fa276f2e1b583b3cee
omnyaahmed959 <omnyaahmed959@gmail.com>
2025-08-07 11:32:23 +0300

    Upgate BrandMVC

---
+++ Efreshli.MVC/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Efreshli.MVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int BrandId { get; set; }  // مفتاح أجنبي
+        public Brand Brand { get; set; }  // Navigation property
+    }
+}
