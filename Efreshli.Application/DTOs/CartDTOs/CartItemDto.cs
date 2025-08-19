2c2146114bd59d032b107cc273b1060070b42382
Doha Ahmed Ezzat <132849296+DohaEzzat@users.noreply.github.com>
2025-08-19 19:59:51 +0300

    cart

---
+++ Efreshli.Application/DTOs/CartDTOs/CartItemDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Efreshli.Application.DTOs.CartDTOs
+{
+    public class CartItemDto
+    {
+        public int CartItemId { get; set; }
+        public int ProductItemId { get; set; }
+        public string ProductName { get; set; } 
+        public decimal Price { get; set; } 
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; } 
+    }
+}
